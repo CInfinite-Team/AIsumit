fa481c2e32b8285c75bf1ec71a6944a7aa47b6c0
gajju44 <gajjunaphade@gmail.com>
2025-08-04 22:49:06 +0530

    fixed infinite scroll of colaborators

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -50,13 +50,7 @@ function Home() {
   { src: Google, alt: 'Google' },
   { src: Oracle, alt: 'Oracle' },
   { src: IBM, alt: 'IBM' },
-    { src: SambaNova, alt: 'SambaNova' },
-    { src: SambaNova, alt: 'SambaNova' },
-    { src: SambaNova, alt: 'SambaNova' },
-    { src: SambaNova, alt: 'SambaNova' },
-    { src: SambaNova, alt: 'SambaNova' },
-    { src: SambaNova, alt: 'SambaNova' },
-
+   
  
 ];
 
